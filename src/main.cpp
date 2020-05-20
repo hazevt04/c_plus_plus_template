2065b7d3a0b1ce3901866e37e9e41dbc30729b93
Glenn Hazelwood <ghazelwood@ltsnet.net>
2020-05-20 13:53:36 -0400

    Forgot to update this from the test_std_move repo. This moves the testing of the my_file_io_funcs to my_file_io_funcs.cpp instead of main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,13 @@ void usage( const char* prog_name ) {
    printf( "\n" );
 }
 
-
 int main( int argc, char** argv ) {
    std::string filename = "foo.bin";
    bool debug = true;
    int num_vals = 10;
-   long long file_size = 0;
-   std::ostringstream err_msg_stream( std::ostringstream::ate );
    bool inject_error = false;
+
+   std::ostringstream err_msg_stream( std::ostringstream::ate );
 
    try {
       if ( argc > 2 ) {
@@ -34,48 +33,19 @@ int main( int argc, char** argv ) {
             throw std::invalid_argument( err_msg_stream.str() );
          }
       }
-
-      float* write_vals = new float[ num_vals * sizeof( float ) ];
-      float* read_vals = new float[ num_vals * sizeof( float ) ];
-
-      gen_vals<float>( write_vals, 100, 0, num_vals );
-      printf( "Write Values:\n" );
-      printf_floats( write_vals, num_vals );
-
-      debug_printf( debug, "The input text file is %s\n", filename.c_str() ); 
-
-      write_binary_floats_file( write_vals, filename.c_str(), num_vals, debug );
-
-      check_file_size( file_size, filename.c_str(), debug ); 
-
-      if ( inject_error ) {
-         filename = "wrong_file.bin";
-      }
-
-      read_binary_floats_file( read_vals, filename.c_str(), num_vals, debug ); 
       
-      printf( "Read Values:\n" );
-      printf_floats( read_vals, num_vals );
-
-      int num_mismatches = 0;
-      if ( !compare_floats( read_vals, write_vals, num_vals ) ) {
-         err_msg_stream <<  "Values read from " << filename 
-            << " don't match values written";      
-         throw std::runtime_error( err_msg_stream.str() );
-      } else {
-         printf( "All %d values read from %s matched the values written\n", 
-               num_vals, filename.c_str() );
-      }
-
       std::string foo_str = "foo";
       double foo_double = 3.141592653589793238462f;
-      debug_cout( debug, "Testing debug_cout: num_vals is ", num_vals,", foo_str is ", foo_str, ", foo_double is ", foo_double );
+      debug_cout( debug, "Testing debug_cout: num_vals is ", 
+            num_vals,", foo_str is ", foo_str, ", foo_double is ", 
+            foo_double );
+
+      test_my_file_io_funcs( filename, num_vals, inject_error, debug );
+      
       return EXIT_SUCCESS;
    
    } catch( std::exception& ex ) {
       printf( "ERROR: %s\n", ex.what() ); 
       return EXIT_FAILURE;
    }
-   
 }
-
